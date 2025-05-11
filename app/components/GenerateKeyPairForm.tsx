6b4a8c7c7306970071547b3f58c81ef20987eaa4
marco-vidali <marcovidali@outlook.com>
2025-05-11 21:07:30 +0200

    Separate server from client rendering

---
+++ app/components/GenerateKeyPairForm.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { generateKeyPair } from "@/utils/pgp";
+import { redirect } from "next/navigation";
+import { SyntheticEvent, useState } from "react";
+
+export default function GenerateKeyPairForm() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [passphrase, setPassphrase] = useState("");
+
+    const handleSubmit = async (e: SyntheticEvent) => {
+        e.preventDefault();
+        await generateKeyPair(name, email, passphrase);
+        redirect("/");
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                placeholder="Name (optional)"
+                onChange={(e) => setName(e.currentTarget.value)}
+            />
+
+            <input
+                type="text"
+                placeholder="E-Mail (optional)"
+                onChange={(e) => setEmail(e.currentTarget.value)}
+            />
+
+            <input
+                type="password"
+                placeholder="Passphrase (optional)"
+                onChange={(e) => setPassphrase(e.currentTarget.value)}
+            />
+
+            <input type="submit" value="Generate" />
+        </form>
+    );
+}
--- app/generate-key-pair/page.tsx
+++ app/generate-key-pair/page.tsx
@@ -1,45 +1,10 @@
-"use client";
-
-import { generateKeyPair } from "@/utils/pgp";
-import { redirect } from "next/navigation";
-import { SyntheticEvent, useState } from "react";
+import GenerateKeyPairForm from "../components/GenerateKeyPairForm";
 
 export default function Page() {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [passphrase, setPassphrase] = useState("");
-
-    const handleSubmit = async (e: SyntheticEvent) => {
-        e.preventDefault();
-        await generateKeyPair(name, email, passphrase);
-        redirect("/");
-    };
-
     return (
         <div>
             <h1>Generate Key Pair</h1>
-
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    placeholder="Name (optional)"
-                    onChange={(e) => setName(e.currentTarget.value)}
-                />
-
-                <input
-                    type="text"
-                    placeholder="E-Mail (optional)"
-                    onChange={(e) => setEmail(e.currentTarget.value)}
-                />
-
-                <input
-                    type="password"
-                    placeholder="Passphrase (optional)"
-                    onChange={(e) => setPassphrase(e.currentTarget.value)}
-                />
-
-                <input type="submit" value="Generate" />
-            </form>
+            <GenerateKeyPairForm />
         </div>
     );
 }
