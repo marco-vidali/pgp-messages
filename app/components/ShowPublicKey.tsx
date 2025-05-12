069332ae7247b3cbb42910b2c51c3d569610686b
marco-vidali <marcovidali@outlook.com>
2025-05-12 17:17:22 +0200

    Create ShowPublicKey component

---
+++ app/components/ShowPublicKey.tsx
@@ -0,0 +1,28 @@
+import { isStoredPublicKeyValid, readStoredPublicKey } from "@/utils/pgp";
+import { redirect } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function ShowPublicKey() {
+    const [publicKey, setPublicKey] = useState("");
+
+    useEffect(() => {
+        const initKeyPair = async () => {
+            if (!(await isStoredPublicKeyValid()))
+                redirect("/generate-key-pair");
+            setPublicKey(await readStoredPublicKey());
+        };
+
+        initKeyPair();
+    }, []);
+
+    return (
+        <>
+            <h1>PGP Test</h1>
+
+            <div>
+                <h2>Your public key</h2>
+                <p>{publicKey}</p>
+            </div>
+        </>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,22 @@
 "use client";
 
-import { isStoredPublicKeyValid, readStoredPublicKey } from "@/utils/pgp";
-import { redirect } from "next/navigation";
-import { useEffect, useState } from "react";
 import Link from "next/link";
+import ShowPublicKey from "./components/ShowPublicKey";
 
 export default function Page() {
-    const [publicKey, setPublicKey] = useState("");
-
-    useEffect(() => {
-        const initKeyPair = async () => {
-            if (!(await isStoredPublicKeyValid()))
-                redirect("/generate-key-pair");
-            setPublicKey(await readStoredPublicKey());
-        };
-
-        initKeyPair();
-    }, []);
-
     return (
-        <div>
-            <h1>PGP Test</h1>
-
-            <div>
-                <h2>Your public key</h2>
-                <p>{publicKey}</p>
-            </div>
+        <>
+            <ShowPublicKey />
 
             <div>
                 <Link href="/encrypt">
                     <button>Encrypt message</button>
                 </Link>
+
                 <Link href="/decrypt">
                     <button>Decrypt message</button>
                 </Link>
             </div>
-        </div>
+        </>
     );
 }
