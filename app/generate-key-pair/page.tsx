24644c083d682f12ef69a7c1a5351fea97e311a1
marco-vidali <marcovidali@outlook.com>
2025-05-12 17:19:04 +0200

    Remove useless div tag

--- app/generate-key-pair/page.tsx
+++ app/generate-key-pair/page.tsx
@@ -2,9 +2,9 @@ import GenerateKeyPairForm from "../components/GenerateKeyPairForm";
 
 export default function Page() {
     return (
-        <div>
+        <>
             <h1>Generate Key Pair</h1>
             <GenerateKeyPairForm />
-        </div>
+        </>
     );
 }
