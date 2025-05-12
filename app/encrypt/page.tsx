ce532cd29167566c9bfdec93e9292bb6187045a3
marco-vidali <marcovidali@outlook.com>
2025-05-12 18:08:13 +0200

    Make encrypt form controlled

--- app/encrypt/page.tsx
+++ app/encrypt/page.tsx
@@ -1,4 +1,12 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Page() {
+    const [recipientPublicKey, setRecipientPublicKey] = useState("");
+    const [message, setMessage] = useState("");
+    const [encryptedMessage, setEncryptedMessage] = useState("");
+
     return (
         <>
             <h1>Encrypt Message</h1>
@@ -7,6 +15,8 @@ export default function Page() {
                 <textarea
                     placeholder="Recipient public key"
                     style={{ flex: 1, height: "100%", resize: "none" }}
+                    value={recipientPublicKey}
+                    onChange={(e) => setRecipientPublicKey(e.target.value)}
                 />
 
                 <div
@@ -19,11 +29,15 @@ export default function Page() {
                     <textarea
                         placeholder="Message"
                         style={{ resize: "none", flex: 1 }}
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
                     />
                     <textarea
                         placeholder="Encrypted message"
                         readOnly
                         style={{ resize: "none", flex: 1 }}
+                        value={encryptedMessage}
+                        onChange={(e) => setEncryptedMessage(e.target.value)}
                     />
                 </div>
             </div>
