01a9c6665086d30e1fa5f055e5ba83f5cd0de6cc
marco-vidali <marcovidali@outlook.com>
2025-05-12 17:55:06 +0200

    Create encrypt message layout

--- app/encrypt/page.tsx
+++ app/encrypt/page.tsx
@@ -1,3 +1,32 @@
 export default function Page() {
-    return <h1>Encrypt Message</h1>;
+    return (
+        <>
+            <h1>Encrypt Message</h1>
+
+            <div style={{ display: "flex", height: "calc(100vh - 100px)" }}>
+                <textarea
+                    placeholder="Recipient public key"
+                    style={{ flex: 1, height: "100%", resize: "none" }}
+                />
+
+                <div
+                    style={{
+                        flex: 1,
+                        display: "flex",
+                        flexDirection: "column",
+                    }}
+                >
+                    <textarea
+                        placeholder="Message"
+                        style={{ resize: "none", flex: 1 }}
+                    />
+                    <textarea
+                        placeholder="Encrypted message"
+                        readOnly
+                        style={{ resize: "none", flex: 1 }}
+                    />
+                </div>
+            </div>
+        </>
+    );
 }
